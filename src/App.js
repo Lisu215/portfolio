a1db06fb576a3b3264f36472558f3fa287aa234b
Lisu215 <kbd8080@gmail.com>
2022-05-16 14:42:30 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -24,15 +25,34 @@ const App = () => {
   };
 
   return (
-    <div>
+    <MovieContainer>
       {movies.map((movie) => (
         <div key={movie.id}>
-          <h1>{movie.title}</h1>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <MovieCard>{movie.title}</MovieCard>
         </div>
       ))}
-    </div>
+    </MovieContainer>
   );
 };
 
 export default App;
+
+const MovieContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+`;
+
+const MovieCard = styled.div`
+  width: 490px;
+  height: 300px;
+  margin: 10px;
+  background-color: #f5f5f5;
+  box-shadow: 0px 0px 10px #000000;
+  display: flex;
+  text-align: center;
+  font-size: 30px;
+  align-items: center;
+  justify-content: center;
+`;
