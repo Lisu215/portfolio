28a833397bc631b96eba943d0df82be59da896d6
Lisu215 <kbd8080@gmail.com>
2022-05-16 11:11:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const API_KEY = `fae1f26c794f675b927cfed2a3fd14f0`;
+  const BASE_URL = `https://api.themoviedb.org/3`;
+  const Trend = `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`;
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const fetchApi = async () => {
+    const response = await axios.get(Trend);
+    setMovies(response.data.results);
+    setLoading(false);
+    console.log(response.data.results);
+  };
+
   return (
-    <Router>
-      <Routes>
-      </Routes>
-    </Router>
+    <div>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h1>{movie.title}</h1>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+        </div>
+      ))}
+    </div>
   );
 };
 
