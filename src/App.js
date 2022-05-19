66b49bf2dbc1ca2edf6b5dd3b680deee02b4cb23
Lisu215 <kbd8080@gmail.com>
2022-05-20 07:10:23 +0900

    검색결과에러 작업

--- src/App.js
+++ src/App.js
@@ -15,17 +15,19 @@ import axios from "axios";
 import Pagination from "react-js-pagination";
 import styled from "styled-components";
 import { FaSearch, FaTimes } from "react-icons/fa";
+import img404 from "./assets/404.png";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
+  const [noFound, setNoFound] = useState(false);
 
   const API_KEY = `fae1f26c794f675b927cfed2a3fd14f0`;
   const BASE_URL = `https://api.themoviedb.org/3`;
   const IMAGE_URL = `https://image.tmdb.org/t/p/w300`;
-  const SEARCH_URL = `${BASE_URL}/search/movie?api_key=${API_KEY}&sort_by=&include_adult=false&query=${search}&language=ko&page=1`;
+  const SEARCH_URL = `${BASE_URL}/search/movie?api_key=${API_KEY}&sort_by=&include_adult=false&query=${search}&language=ko&page=`;
 
   useEffect(() => {
     const endpoint = `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=ko&page=${page}`;
@@ -46,18 +48,24 @@ const App = () => {
     window.scrollTo({
       top: 0,
     });
+    if (search) {
+      const endpoint = `${SEARCH_URL}${pageNumber}`;
+      fetchApi(endpoint);
+    }
   };
 
   const handleSearch = async (e) => {
     e.preventDefault();
-    const endpoint = `${SEARCH_URL}${search}&page=${page}`;
+    const endpoint = `${SEARCH_URL}${page}`;
     const response = await axios.get(endpoint);
     setMovies([...response.data.results]);
     setLoading(false);
-    setSearch("");
     window.scrollTo({
       top: 0,
     });
+    if (response.data.results.length === 0) {
+      setNoFound(true);
+    }
   };
 
   return (
@@ -70,67 +78,78 @@ const App = () => {
           <Form className="d-flex">
             <FormControl
               type="search"
-              placeholder="Search"
-              className="me-2"
+              placeholder="검색"
+              className="me-3"
               aria-label="Search"
               value={search}
-              onChange={(e) => setSearch(e.target.value)}
+              onChange={(e) => {
+                setSearch(e.target.value);
+                setPage(1);
+              }}
             />
             <Button onClick={handleSearch}>Search</Button>
           </Form>
         </Container>
       </Navbar>
-      <Container>
-        <Row className="justify-content-md-center">
-          {movies.map((movie) => (
-            <Col
-              key={movie.id}
-              xs={12}
-              md={3}
-              style={{
-                padding: 20,
-              }}
-            >
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={`${IMAGE_URL}${movie.poster_path}`}
-                  alt={movie.id}
-                  style={{
-                    height: 350,
-                  }}
-                />
-                <Card.Body>
-                  <Card.Title>
-                    {movie.title
-                      ? movie.title.length > 20
-                        ? movie.title.substring(0, 20) + "..."
-                        : movie.title
-                      : "Untitled"}
-                  </Card.Title>
-                  <Card.Text
+
+      {noFound == true ? (
+        <div className="no-found">
+          "{search}"의 검색 결과를 찾을 수 없습니다.
+          <img src={img404} alt="notfound" />
+        </div>
+      ) : (
+        <Container>
+          <Row className="justify-content-md-center">
+            {movies.map((movie) => (
+              <Col
+                key={movie.id}
+                xs={12}
+                md={3}
+                style={{
+                  padding: 20,
+                }}
+              >
+                <Card>
+                  <Card.Img
+                    variant="top"
+                    src={`${IMAGE_URL}${movie.poster_path}`}
+                    alt={movie.id}
                     style={{
-                      height: 150,
+                      height: 350,
                     }}
-                  >
-                    {movie.overview.length > 130
-                      ? movie.overview.substring(0, 130) + "..."
-                      : movie.overview}
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-        <Pagination
-          activePage={page}
-          totalItemsCount={200}
-          pageRangeDisplayed={5}
-          prevPageText={"‹"}
-          nextPageText={"›"}
-          onChange={handlePageChange}
-        />
-      </Container>
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      {movie.title
+                        ? movie.title.length > 20
+                          ? movie.title.substring(0, 20) + "..."
+                          : movie.title
+                        : "Untitled"}
+                    </Card.Title>
+                    <Card.Text
+                      style={{
+                        height: 150,
+                      }}
+                    >
+                      {movie.overview.length > 130
+                        ? movie.overview.substring(0, 130) + "..."
+                        : movie.overview}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          <Pagination
+            activePage={page}
+            totalItemsCount={200}
+            pageRangeDisplayed={5}
+            prevPageText={"‹"}
+            nextPageText={"›"}
+            onChange={handlePageChange}
+          />
+        </Container>
+      )}
     </div>
   );
 };
@@ -151,21 +170,3 @@ const Button = styled.button`
     color: #fff;
   }
 `;
-
-/*
-const Search = styled.input`
-  width: 20%;
-  height: 40px;
-  margin-right: 10px;
-  border-radius: 5px;
-  border: 2px solid #000;
-  padding: 0 20px;
-  font-size: 16px;
-  outline: none;
-  &:focus {
-    border: 2px solid #337ab7;
-  }
-`;
-<FaSearch size="19" className="icon" />
-          <Search />
-*/
